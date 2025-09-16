3cd1a4be537ca0b3fefd82653bc97d94b6581942
Patrik Macák <patrik.macak93@gmail.com>
2025-09-16 20:30:00 +0200

    feat: add sumRange & isPrime into w1 excersises

---
+++ exercises/w1/isPrime.js
@@ -0,0 +1,11 @@
+let inputNumber = 1;
+let dividePrevious = 0;
+if (inputNumber < 1){
+  console.log('false');
+} else if (inputNumber === 1){
+  console.log('true');
+} else if (Number.isInteger) {
+  for (let i = 0; i <= inputNumber;i++){
+    
+  }
+}
---
+++ exercises/w1/sumRange.js
@@ -0,0 +1,6 @@
+let N = 2;
+let sumRange = 0;
+for (let i = 0; i <= N; i++) {
+  sumRange += i;
+}
+console.log(sumRange);
